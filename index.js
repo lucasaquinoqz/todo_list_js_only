0318ed8d87459d13c82a2193e1d668d0cee0f18e
lucasaquinoqz <lucasaquinoq@gmail.com>
2023-08-08 13:19:59 -0300

    Adicionando menu inicial, com o readline, mas não tem como usar ele com caracteres especiais no momento

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-function teste() {
-    console.log("teste")
-}
+const readline = require('readline-sync')
+
+let opcao = 0
+do {
+    console.log("Bem vindo ao Todo List!")
+    console.log(`
+        Informe o que você deseja fazer:
+        1- Adicionar novo afazer
+        2- Ver os afazeres
+        3- Alterar um afazer
+        4- Marcar um afazer como concluido
+        5- Apagar um afazer
+        6- Sair
+    `);
+
+    opcao = Number(readline.question("Escolha uma opcao: "))
+
+} while (opcao != 6)
+
+
+console.log("Obrigado e até mais!")
